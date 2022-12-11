7da8018c834cf0aa42fa7a23c6fe44bb30c2e9ce
uditra <yehudaar@g.jct.ac.il>
2022-12-11 21:04:29 +0200

    editing

--- src/component/EditInpt.js
+++ src/component/EditInpt.js
@@ -1,32 +1,30 @@
 import React, {useState} from "react";
-import { v4 as uuidv4 } from 'uuid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
-const InputTodo = ({addTodo}) => {
+const EditTodo = ({editTodo ,todoItem}) => {
 
-    const [todo,setTodo] =useState({title: "",date:"",flag:false,id:uuidv4()})
+    const [todo,setTodo] =useState({title: todoItem.title,date:todoItem.date,flag:todoItem.flag,id:todoItem.id})
 
     const handleSubmit = e => {
         console.log(todo);
         e.preventDefault()
         if (!todo.title || !todo.date) return
-        addTodo(todo)
-        setTodo({...todo, title: "",date:"",id:uuidv4()})
+        editTodo(todo)
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <Stack spacing={2} direction="row" justifyContent="center"> 
                 <TextField id="outlined-basic" label="name your new task" variant="outlined"
-                type="text" value={todo.title} placeholder="name your new task" 
+                type="text" value={todo.title} placeholder={todo.title} 
                 onChange={e => setTodo(todo =>({...todo,title: e.target.value}))}/>
-                <input type="date" value={todo.date} placeholder="" 
+                <input type="date" value={todo.date} placeholder={todo.date} 
                 onChange={e => setTodo(todo =>({...todo,date: e.target.value}))}/>
-                <Button variant="contained" type="submit">add Todo item</Button>
+                <Button variant="contained" type="submit">close editing</Button>
             </Stack>
         </form>
     )
 }
-export default InputTodo
+export default EditTodo
--- src/component/TodoListContainer.js
+++ src/component/TodoListContainer.js
@@ -2,9 +2,12 @@ import React, {useState, useEffect} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {fetchTodoList , addTodoItems} from '../redux/TodoListActions'
 import InputTodo from "./inputTodo";
+import EditInput from "./EditInpt"
 
 const TodoListContainer = () => {
 
+    const [currentTodo, setCurrentTodo] = useState({});
+    const [isEditing, setIsEditing] = useState(false);
     const TodoListData = useSelector(state => state)
     const dispatch = useDispatch()
 
@@ -16,6 +19,20 @@ const TodoListContainer = () => {
         dispatch(addTodoItems(todoItem))
     }
 
+    const editTodoItem = (todoItem) => {
+        setIsEditing(false)
+        setCurrentTodo({})
+    }
+
+    const handleEditClick =(todo) => {
+        // set editing to true
+        setIsEditing(true);
+        // set the currentTodo to the todo item that was clicked
+        setCurrentTodo({ ...todo });
+        console.log(isEditing);
+        console.log(currentTodo);
+      }
+
     return TodoListData.loading ? (
         <h1>loading data</h1>
     ) : TodoListData.error ? (
@@ -23,11 +40,18 @@ const TodoListContainer = () => {
     ): (
         <div>
             <h1>TodoList</h1>
-            <div>
-                {TodoListData && 
-                TodoListData &&
-                TodoListData.TodoList.map(todo => <div key={todo.id}><p>{todo.title}</p></div>)}
-            </div>
+            <ul className="todo-list">
+                {TodoListData.TodoList.map(todo => 
+                (!isEditing || currentTodo.id !== todo.id )?(
+                    <li key={todo.id}>
+                        <p>task: {todo.title} date: {todo.date} </p>
+                        {/* we are passing the entire todo object to the handleEditClick function*/}
+                        <button onClick={() => handleEditClick(todo)}>Edit</button>
+                        <button >Delete</button>
+                    </li>
+                ):(<EditInput editTodo={editTodoItem} todoItem={todo}/>)
+                )}
+            </ul>
             <InputTodo addTodo={addTodoItem}/>
         </div>
     )
