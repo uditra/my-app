46c208e8cd5e50d72695eaa6dfebd32d94723ffb
uditra <yehudaar@g.jct.ac.il>
2022-12-11 13:31:10 +0200

    MUI

--- src/component/inputTodo.js
+++ src/component/inputTodo.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
-
 import { v4 as uuidv4 } from 'uuid';
-
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 const InputTodo = ({addTodo}) => {
 
@@ -17,11 +18,15 @@ const InputTodo = ({addTodo}) => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" value={ todo.title} placeholder="add new task" 
-            onChange={e => setTodo(todo =>({...todo,title: e.target.value}))}/>
-            <input type="date" value={todo.date} placeholder="" 
-            onChange={e => setTodo(todo =>({...todo,date: e.target.value}))}/>
-            <button type="submit">add Todo item</button>
+            <Stack spacing={2} direction="row" justifyContent="center"> 
+                <TextField id="outlined-basic" label="name your new task" variant="outlined">
+                <input type="text" value={ todo.title} placeholder="name your new task" 
+                onChange={e => setTodo(todo =>({...todo,title: e.target.value}))}/>
+                </TextField>
+                <input type="date" value={todo.date} placeholder="" 
+                onChange={e => setTodo(todo =>({...todo,date: e.target.value}))}/>
+                <Button variant="contained" type="submit">add Todo item</Button>
+            </Stack>
         </form>
     )
 }
